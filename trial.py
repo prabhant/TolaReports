c5fb3e714ebb10a274d7fb17766f8cdc33f10bb4
prabhant <prabhantsingh@gmail.com>
2018-07-03 15:45:20 +0200

    plotly tables plus figure
    plotly figures instead of html tables

--- trial.py
+++ trial.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
 
 app = dash.Dash()
 
@@ -31,10 +32,8 @@ app.layout = html.Div([
         id='dropdown'
     ),
     dcc.Graph(id = 'my-graph'),
-    html.Div(children=[
-        html.H4(children='US Agriculture Exports (2011)'),
-        generate_table(df.head())
-        ])
+    dcc.Graph(id = 'my-table'),
+    dcc.Graph(id = 'groupby-table')
 
     ])
 
@@ -67,7 +66,33 @@ def update_graph(value):
         ]
     }
 
+@app.callback(Output('my-table', 'figure'), [Input('dropdown', 'value')])
+def update_table(value):
+    df = pd.read_csv('data.csv')
+    y1 = []
+    y2 = []
+    indname = []
+    for i in range(len(df)):
+        if (df['periodic_target'].iloc[i] == value):
+            indname.append(df['name'].iloc[i])
+            y1.append(df['achieved'].iloc[i])
+            y2.append(df['lop_target'].iloc[i])
+
+    dff = pd.DataFrame({'Indicator name': indname, 'achieved': y1, 'target': y2})
+    df_fig = ff.create_table(dff)
+    return df_fig
+
+@app.callback(Output('groupby-table', 'figure'), [Input('dropdown', 'value')])
+def update_groupby_table(value):
+    df_combine = pd.read_csv('combine.csv')
+    df_combine = df_combine[df_combine['Month'] == value]
+    dfc = df_combine.groupby(['Gender', 'Age Category', 'Activity']).size()
+    dfc = pd.DataFrame(dfc)
+    dfc.to_csv('dfc.csv')
+    dff = pd.read_csv('dfc.csv')
+    new_tab = ff.create_table(dff)
+    return new_tab
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8550)
+    app.run_server(debug=True, port=8560)
