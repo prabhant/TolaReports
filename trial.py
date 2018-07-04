b496e6fd183177c724d58f8c4e605f1d123ba210
prabhant <prabhantsingh@gmail.com>
2018-07-04 18:06:03 +0200

    New data
    added another table with the new data supplied

--- trial.py
+++ trial.py
@@ -26,18 +26,25 @@ def generate_table(dataframe, max_rows=10):
 
 app.layout = html.Div([
     dcc.Dropdown(
-        value=['a'],
+        value=[],
         options=[{'label': i, 'value': i} for i in list(a)],
         multi=False,
-        id='dropdown'
+        id='dropdown-month'
+    ),
+    dcc.Dropdown(
+        value=['2017'],
+        options=[{'label': i, 'value': i} for i in ['2017','2018']],
+        multi=False,
+        id='dropdown-year'
     ),
     dcc.Graph(id = 'my-graph'),
     dcc.Graph(id = 'my-table'),
-    dcc.Graph(id = 'groupby-table')
+    dcc.Graph(id = 'groupby-table'),
+    dcc.Graph(id = 'new-table')
 
     ])
 
-@app.callback(Output('my-graph', 'figure'), [Input('dropdown', 'value')])
+@app.callback(Output('my-graph', 'figure'), [Input('dropdown-month', 'value')])
 def update_graph(value):
     df = pd.read_csv('data.csv')
     y1 = []
@@ -55,6 +62,7 @@ def update_graph(value):
             name = 'achieved',
             marker = go.Marker(
                 color = 'rgb(55,83,109)'
+
             )
 
         ),
@@ -66,7 +74,7 @@ def update_graph(value):
         ]
     }
 
-@app.callback(Output('my-table', 'figure'), [Input('dropdown', 'value')])
+@app.callback(Output('my-table', 'figure'), [Input('dropdown-month', 'value')])
 def update_table(value):
     df = pd.read_csv('data.csv')
     y1 = []
@@ -82,7 +90,7 @@ def update_table(value):
     df_fig = ff.create_table(dff)
     return df_fig
 
-@app.callback(Output('groupby-table', 'figure'), [Input('dropdown', 'value')])
+@app.callback(Output('groupby-table', 'figure'), [Input('dropdown-month', 'value')])
 def update_groupby_table(value):
     df_combine = pd.read_csv('combine.csv')
     df_combine = df_combine[df_combine['Month'] == value]
@@ -92,6 +100,45 @@ def update_groupby_table(value):
     dff = pd.read_csv('dfc.csv')
     new_tab = ff.create_table(dff)
     return new_tab
+
+
+@app.callback(Output('new-table', 'figure'), [Input('dropdown-year', 'value'),Input('dropdown-month', 'value')])
+def update_new_tab(year, month):
+    year = int(year)
+    df = pd.read_csv('new_report.csv')
+    df = df.drop(df.columns[31], axis=1)
+    df = df.drop(df.columns[31], axis=1)
+    df = df.drop(df.columns[31], axis=1)
+    month_arr = ['Month_ID_Mob', 'Month_N_Mgt', 'Month_Demo', 'Month_PHH',
+                 'Month_Mrk_Sales', 'Month_C_Mgt', 'Month_Seed_Dist']
+    year_arr = ['Year_ID_Mob', 'Year_N_Mgt', 'Year_Demo', 'Year_PHH',
+                'Year_Mrk_Sales', 'Year_C_Mgt', 'Year_Seed_Dist']
+    activity_arr = ['id mob', 'nursery management', 'demo', 'phh', 'marketing', 'crop management', 'seed distribution']
+    count_arr = []
+    df_arr = []
+    index = 10
+
+    val = month
+    valy = year
+    for i in range(len(month_arr)):
+        index = index + 3
+        df_tmp = df[df[month_arr[i]] == val]
+        df_tmp = df_tmp[df_tmp[year_arr[i]] == valy]
+        count_tmp = df_tmp.iloc[:, index].value_counts()
+        count_arr.append(count_tmp)
+        df_arr.append(df_tmp)
+    df_final = pd.DataFrame()
+    for i in range(len(activity_arr)):
+        dfc = df_arr[i]
+        dfc = dfc.groupby(['Gender', 'Age cohorts']).size()
+        dfc = pd.DataFrame(dfc)
+        dfc.insert(loc=0, column='Activity', value=activity_arr[i])
+        df_final = df_final.append(dfc)
+    df_final.to_csv('tmp.csv')
+    df_display = pd.read_csv('tmp.csv')
+    df_display = df_display[['Activity', 'Gender', 'Age cohorts', '0']]
+    tab = ff.create_table(df_display)
+    return  tab
 
 
 if __name__ == '__main__':
